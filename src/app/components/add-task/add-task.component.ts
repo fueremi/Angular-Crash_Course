7ed04c1935f4b89122fed3b9e987f60a6ebf1c9d
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-05-16 23:48:45 +0700

    add ui service to do something on UI by spesific action

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Task } from '../../Task'
+import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -7,33 +9,38 @@ import { Task } from '../../Task'
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
-  @Output() onAddTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor() {}
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {}
 
   onSubmit() {
-    if ( !this.text ) {
-      alert('Please add a task!')
-      return
+    if (!this.text) {
+      alert('Please add a task!');
+      return;
     }
 
     const newTask = {
       text: this.text,
       day: this.day,
-      reminder: this.reminder
-    } 
+      reminder: this.reminder,
+    };
 
     // TODO - Emit event
-    this.onAddTask.emit(newTask)
+    this.onAddTask.emit(newTask);
 
     this.text = '';
     this.day = '';
     this.reminder = false;
-
   }
 }
